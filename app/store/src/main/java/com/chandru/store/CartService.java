2f9f08296960cde217aba845ce440244ece32486
Chandru M G <chandrumg124@gmail.com>
2026-06-19 03:48:05 +0530

    intentinally replaced + with - in cart to check build failure

--- app/store/src/main/java/com/chandru/store/CartService.java
+++ app/store/src/main/java/com/chandru/store/CartService.java
@@ -3,6 +3,6 @@ package com.chandru.store;
 public class CartService {
 
     public int addQuantity(int currentQty, int qtyToAdd) {
-        return currentQty + qtyToAdd;
+        return currentQty - qtyToAdd;
     }
 }
